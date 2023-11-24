51da3a7d41ccf565934fc01f18ce315290f45f23
Maya <maya.cecili@gmail.com>
2023-11-24 20:34:12 +0100

    added script code

--- script.js
+++ script.js
@@ -7,16 +7,13 @@ const dayOut = document.querySelector('#days');
 const monthOut = document.querySelector('#months');
 const yearOut = document.querySelector('#years');
 //Other elements
-const form = document.querySelector("#form");
+const form = document.getElementById("form");
 const small = document.querySelector("small");
 const container = document.querySelector(".container");
 const submit = document.querySelector('#submit');
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
-console.log("this is working")
-    validateInput();
-});
+submit.addEventListener('click', validateInput);
+    
 
 let date = new Date();
 let getDay = date.getDate();
@@ -26,24 +23,55 @@ let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 
 function validateInput(){
-    const inputs = document.querySelectorAll('input');
-    inputs.forEach((input) => {
-        if(input.value === ''){
-            container.className ='container error';
-            small.style.visibility = 'visible';
-            small.innerText = "This field is required.";
-        }else if(monthInput.value > 12){
-            container.className ='container error';
-            small.style.visibility = 'visible';
-            small.innerText = "Must be a valid month.";
-        }else if(dayInput.value > 31){
-            container.className ='container error';
-            small.style.visibility = 'visible';
-            small.innerText = "Must be a valid day."; 
-        }else{
-            container.className ='container';
-            small.style.visibility = 'hidden';
-            small.innerText = "";
+        if(dayInput.value > getDay){
+            getDay = getDay + months[getMonth - 1];
+            getMonth = getMonth - 1;  
         }
-});
+        if (monthInput.value > getMonth){
+            getMonth = getMonth + 12;
+            getYear = getYear - 1;
+        }
+    let dayCalc = getDay - dayInput.value;
+    let monthCalc = getMonth - monthInput.value;
+    let yearCalc = getYear - yearInput.value;
+
+        if(dayInput.value === ''){
+            errorFor(dayInput, 'This field is required');
+        }else if(dayInput.value > 31){
+            errorFor(dayInput, 'Must be a valid day')   
+        }else{
+            succesFor(dayInput);
+           dayOut.innerHTML = dayCalc; 
+        }
+        
+        if(monthInput.value === ''){
+            errorFor(monthInput, 'This field is required');
+        }else if(monthInput.value > 12){
+            errorFor(monthInput, 'Must be a valid month')   
+        }else{
+        succesFor(monthInput);
+        monthOut.innerHTML = monthCalc;
+       }
+
+        if(yearInput.value === ''){
+            errorFor(yearInput, 'This field is required');
+        }else if(yearInput.value < 1900 || yearInput.value > 2023){
+            errorFor(yearInput, 'Must be a valid month');
+        }else{
+            succesFor(yearInput);
+            yearOut.innerHTML = yearCalc;
+        } 
+    
+console.log(dayCalc + "and" + monthCalc + "and" + yearCalc)
+}
+
+function errorFor(input, message){
+    const formElement = input.parentElement;
+    formElement.className ='box error';
+    small.innerHTML = message;
+}
+
+function succesFor(input){
+    const formElement =input.parentElement;
+    formElement.className= 'box';
 }
